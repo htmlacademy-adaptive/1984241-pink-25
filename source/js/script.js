e245d0afde28e3cf1ba2452b1c2f224a55700e07
arsenomarov <sen.77@mail.ru>
2022-05-12 00:07:07 +0300

    блок advantages-переделка + js-бургер меню

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+let page = document.querySelector('.main-nav__list');
+let themeButton = document.querySelector('.main-nav__toggle');
+themeButton.onclick = function() {
+  page.classList.toggle('main-nav__list--closed');
+  page.classList.toggle('main-nav__list--opened');
+};
+
+// const btnToggle = document.querySelector('.main-header__toggle');
+const mainHeader = document.querySelector('.main-nav');
+
+themeButton.addEventListener('click', function() {
+themeButton.classList.toggle('main-header__toggle--active');
+mainHeader.classList.toggle('main-header__top--active');
+})
+     
